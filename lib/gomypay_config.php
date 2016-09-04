a03696f786ee575226809af58cb08f4d2f0b35da
jimmy8646 <yujiecheuh@gmail.com>
2016-09-05 00:43:09 +0800

    init

---
+++ lib/gomypay_config.php
@@ -0,0 +1,7 @@
+define('GOMYPAY_DEV_E_NO', 'xxxxxxxx');//測試商店代號
+define('GOMYPAY_E_NO', 'xxxxxxxx');//正式商店代號
+define('GOMYPAY_E_STORENAME', 'xxxxxxxx');
+define('GOMYPAY_DEV_KEY', 'xxxxxxxx');//測試交易驗證密碼
+define('GOMYPAY_KEY', 'xxxxxxxx');//正式交易驗證密碼
+define('GOMYPAY_TEST_URL', 'http://test.gomypay.asia/Shopping/creditpay.asp');//測試網址
+define('GOMYPAY_PRO_URL', 'https://gomypay.asia/Shopping/creditpay.asp');//正式網址
